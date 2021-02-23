81212bb419cc1a54eb699b387bd826da02061fac
Oleksandra Sherstiuk <osherstiuk@e1r10p5.unit.ua>
2021-02-23 20:43:23 +0200

    added some changes

--- src/Questions.js
+++ src/Questions.js
@@ -5,9 +5,12 @@ function Questions() {
     const [data, setData] = useState({});
 
     const url = 'https://api.stackexchange.com/2.2/questions?site=stackoverflow&key=wud)gxqaQ5vssDmltw6d1A((';
-    useEffect(async () => {
-        const result = await axios(url);
-        setData(result.data.items);
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(url);
+            setData(result.data);
+        }
+        fetchData();
     }, []);
 
     console.log(data);
@@ -15,14 +18,23 @@ function Questions() {
 
     return (
         <>
-        {data.map(item => (
+        {(data.length) ? 
+            data.items.map(item => (
+                <div key={item.question_id}>
+                    <p>{item.title}</p>
+                </div>
+            ))
+        :
+            <h1>PRIVET</h1>
+        }
+        {/* {data.items.map(item => (
             <div key={item.question_id}>
                 <p>{item.title}</p>
             </div>
-        ))}
+        ))} */}
+        {/* <h1>PRIVET</h1> */}
         </>
     )
 }
 
-export default Questions
-;
+export default Questions;
